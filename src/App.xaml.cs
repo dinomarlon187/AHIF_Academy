fe7e4036428b2fd4b0843fadf6620eb5c9fefb3f
dinomarlon187 <marlon.pichler@student.htl-rankweil.at>
2024-05-12 21:56:37 +0200

    Neue Solution, ohne MAUI

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,12 +1,14 @@
-﻿namespace AHIF_Academy
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ahif_academy
 {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
     public partial class App : Application
     {
-        public App()
-        {
-            InitializeComponent();
-
-            MainPage = new AppShell();
-        }
     }
+
 }
