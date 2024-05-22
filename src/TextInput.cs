626671265b950831056f91b2780b2b25a27d1f19
dinomarlon187 <marlon.pichler@student.htl-rankweil.at>
2024-05-22 10:16:20 +0200

    Draw function zum testen hinzugefügt

--- src/TextInput.cs
+++ src/TextInput.cs
@@ -26,23 +26,63 @@ namespace ahif_academy
             {
                 Text = Text,
                 FontSize = 20,
-                TextWrapping = System.Windows.TextWrapping.Wrap
+                TextWrapping = System.Windows.TextWrapping.Wrap,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
             };
+            Grid.SetColumn(textBlock, 0);
+            Grid.SetRow(textBlock, 0);
+            Grid.SetColumnSpan(textBlock, 3);
+
+
             TextBox textBox = new TextBox()
             {
                 Height = 50,
                 Width = 100,
-                Text = FalseAnswer
+                Text = FalseAnswer,
+                HorizontalAlignment = System.Windows.HorizontalAlignment.Center,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center
             };
+            Grid.SetColumn(textBox, 0);
+            Grid.SetRow(textBox, 1);
+            Grid.SetColumnSpan(textBox, 3);
+
+
             Button submit = new Button()
             {
                 Height = 50,
                 Width = 100,
                 Content = "Submit"
             };
+            Grid.SetColumn(submit, 1);
+            Grid.SetRow(submit, 2);
+        
+            submit.Click += Submit_Click;
             grid.Children.Add(textBlock);
             grid.Children.Add(textBox);
             grid.Children.Add(submit);
+        }
+
+        private void Submit_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            
+            if (sender is Button)
+            {
+                Button button = (Button)sender;
+                Grid grid = (Grid)button.Parent;
+                TextBox textBox = (TextBox)grid.Children[1];
+                button.IsEnabled = false;
+                textBox.IsReadOnly = true;
+                if (CheckAnswer(textBox.Text.Trim()))
+                {
+                    textBox.Background = System.Windows.Media.Brushes.Green;
+                }
+                else
+                {
+                    textBox.Background = System.Windows.Media.Brushes.Red;
+                    textBox.ToolTip = CorrectAnswer;
+                }
+            }
         }
     }
 }
