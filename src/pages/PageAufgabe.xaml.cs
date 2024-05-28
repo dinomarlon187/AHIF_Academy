9017dbbf5e15022f561fe82e8d9aa90fa3450e40
dinomarlon187 <marlon.pichler@student.htl-rankweil.at>
2024-05-28 21:19:32 +0200

    Vorläufig spieltauglich gemacht

--- src/pages/PageAufgabe.xaml.cs
+++ src/pages/PageAufgabe.xaml.cs
@@ -20,9 +20,25 @@ namespace ahif_academy.pages
     /// </summary>
     public partial class PageAufgabe : Page
     {
+        QuestionList QuestionList = new QuestionList();
+        Question CurrentQuestion;
         public PageAufgabe()
         {
             InitializeComponent();
+            QuestionList.DeserializeFromJSON();
+            foreach (Question question in QuestionList)
+            {
+                question.btnNextQuestion = btnNextQuestion;
+            }
+            CurrentQuestion = QuestionList.GetRandomQuestion();
+            CurrentQuestion.Draw(grid);
+        }
+
+        private void btnNextQuestion_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentQuestion = QuestionList.GetRandomQuestion();
+            CurrentQuestion.Draw(grid);
+            btnNextQuestion.Visibility = Visibility.Hidden;
         }
     }
 }
